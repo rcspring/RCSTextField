3e494012c0ffcbb784e2401ab8e1159ae5130ff7
Ryan Spring <rcspring@gmail.com>
2017-04-05 23:18:00 +0200

    reorg to framework

---
+++ RCSTextField/RCSTextField.h
@@ -0,0 +1,19 @@
+//
+//  RCSTextField.h
+//  RCSTextField
+//
+//  Created by Ryan Spring on 4/5/17.
+//  Copyright © 2017 Ryan Spring. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for RCSTextField.
+FOUNDATION_EXPORT double RCSTextFieldVersionNumber;
+
+//! Project version string for RCSTextField.
+FOUNDATION_EXPORT const unsigned char RCSTextFieldVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <RCSTextField/PublicHeader.h>
+
+
